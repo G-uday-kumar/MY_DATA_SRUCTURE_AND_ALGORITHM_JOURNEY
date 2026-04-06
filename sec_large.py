84e0fa1a438aaaa6c893c20408ef9f7085dd999a
G_Uday_kumar <udayg280604@gmail.com>
2026-04-06 21:05:59 +0530

    Added Basic DSA programs

---
+++ sec_large.py
@@ -0,0 +1,11 @@
+arr1=[12,22,43,23,54,56]
+lar_val1=arr1[0]
+sec_lar_val1=arr1[0]
+for num1 in arr1:
+    if num1 > lar_val1:
+        sec_lar_val1=lar_val1
+        lar_val1=num1
+    elif num1 < lar_val1 and num1 > sec_lar_val1:
+        sec_lar_val1=num1
+print("Second Large is:-",sec_lar_val1)
+
